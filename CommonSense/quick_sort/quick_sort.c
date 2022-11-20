ded571949fb165023215728163657cfcef7fbbf6
ntk221 <nittakdub@gmail.com>
2022-11-20 09:04:13 +0900

    fix return's type

--- CommonSense/quick_sort/quick_sort.c
+++ CommonSense/quick_sort/quick_sort.c
@@ -16,35 +16,55 @@ void	swap(int* a, int* b)
 	*b = t;
 }
 
-/* size_t	partition(int arr[], size_t left, size_t right)
+int	partition(int arr[], int left, int right)
 {
-	size_t	pivot_index;
+	int	pivot_index;
 	int	pivot;
-	size_t	i, j;
 
-	i = left;
-	j = right;
-	pivot = right;
-	j--;
+	pivot_index = right;
+	pivot = arr[right];
+	right--;
 	while (true)
 	{
-		while (arr[i] < arr[pivot])
-			i++;
-		while (arr[j] > arr[pivot])
-			j--;
+		while (arr[left] < pivot)
+			left++;
+		while (arr[right] > pivot)
+			right--;
 		if (left >= right)
 			break ;
 		else
 		{
-			swap(&arr[i], &arr[j]);
-			i++;
+			swap(&arr[left], &arr[right]);
+			left++;
 		}
 	}
-	swap(&arr[i], &arr[pivot]);
-	return i;
+	swap(&arr[left], &arr[pivot_index]);
+	return left;
 }
-*/ 
+ 
+void	quick_sort(int	arr[], int left, int right)
+{
+	size_t	pivot_index;
 
+	if (left - right <= 0) // base case
+	{
+		pivot_index = partition(arr, left, right);
+		quick_sort(arr, left, pivot_index - 1); // 左の部分配列を再帰的に解く
+		quick_sort(arr, pivot_index + 1, right); // 右の部分配列について再帰的に解く
+	}
+	
+}
+
+int	main(void)
+{
+	int     test1[6] = {0, 5, 2, 1, 6, 3};
+	quick_sort(test1, 0, 5);
+	 for (size_t i = 0; i < 6; i++)
+		printf("%d ", test1[i]);
+	return (0);
+}
+
+/* 
 int partition (int array[], int left, int right) {
   int i, j, pivot;
   i = left;
@@ -62,27 +82,9 @@ int partition (int array[], int left, int right) {
 
   return j;
 }
-
-/* void	quick_sort(int	arr[], size_t left, size_t right)
-{
-	size_t	pivot_index;
-	printf("left: %d\n", left);
-	printf("right: %d\n", right);
-	// for (size_t i = 0; i < 6; i++)
-	//	printf("%d ", test1[i]);
-	puts("");
-
-	if (right - left > 0) // base case
-	{
-		pivot_index = partition(arr, left, right);
-		// printf("pivot_index %d\n", pivot_index);
-		quick_sort(arr, left, pivot_index - 1); // 左の部分配列を再帰的に解く
-		quick_sort(arr, pivot_index + 1, right); // 右の部分配列について再帰的に解く
-	}
-	
-}
 */
 
+/*
 void quick_sort (int array[], int left, int right) {
   int pivot;
 
@@ -92,13 +94,4 @@ void quick_sort (int array[], int left, int right) {
     quick_sort(array, pivot+1, right);
   }
 }
-
-
-int	main(void)
-{
-	int     test1[6] = {0, 5, 2, 1, 6, 3};
-	quick_sort(test1, 0, 5);
-	 for (size_t i = 0; i < 6; i++)
-		printf("%d ", test1[i]);
-	return (0);
-}
+*/
